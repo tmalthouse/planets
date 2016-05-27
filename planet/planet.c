babaa9cb37414db3871e0e5a21375172cd8d9ecf
tmalthouse <tmalthouse@gmail.com>
2016-05-27 08:19:38 -0500

    Improve gforce algorithm

--- planet/planet.c
+++ planet/planet.c
@@ -26,12 +26,21 @@ Has O(n^2) speed--Will get slow with lots of bodies--I'll use a better algorithm
 void calc_forces (Darray_CBody *bodies)
 {
     CBody *planets = (bodies->data);
+    Vector2d forcetable[bodies->len][bodies->len];
+
+    for (int i=0; i<bodies->len; i++) {
+        forcetable[i][i] = NULL_VECT;//Helps with debugging
+        for (int j=i+1; j<bodies->len; j++) {
+            Vector2d force = gforce(planets[i], planets[j]);
+            forcetable[i][j] = force;
+            forcetable[j][i] = force;
+        }
+    }
     for (int i=0; i<bodies->len; i++) {
         planets[i].fnet = (Vector2d){0, 0};//Set force to 0
         for (int j=0; j<bodies->len; j++) {
             if (i!=j) {//We really don't want to calc force to ourself--div/0 error
-                vdprintf("Force on body %s from body %s is %f\n", planets[i].name, planets[j].name, vectabs(gforce(planets[i], planets[j]), (Vector2d){0,0}))
-                addforce (planets+i, gforce(planets[i], planets[j]));
+                addforce (planets+i, forcetable[i][j]);
             }
         }
         vdprintf("Net force on body %s is %f\n", planets[i].name, vectabs(planets[i].fnet, (Vector2d){0,0}));
@@ -43,7 +52,7 @@ void calc_forces (Darray_CBody *bodies)
 void cbody_update (Darray_CBody *bodies, double dt)
 {
     calc_forces (bodies);
-    
+
     //First, we change each body's velocity, using the forces calculated a line ago
     CBody *planets = bodies->data;
     for (int i=0; i<bodies->len; i++) {
@@ -51,7 +60,7 @@ void cbody_update (Darray_CBody *bodies, double dt)
         acc = (Vector2d){planets[i].fnet.x/planets[i].mass, planets[i].fnet.y/planets[i].mass};
         planets[i].vel = vectadd(planets[i].vel, vfmult(acc, dt));
     }
-    
+
     //And then we change their positions based on their new velocities
     for (int i=0; i<bodies->len; i++) {
         planets[i].pos = vectadd(planets[i].pos, vfmult(planets[i].vel, dt));
@@ -60,12 +69,12 @@ void cbody_update (Darray_CBody *bodies, double dt)
 
 Coordinate absmaxpos (Darray_CBody *bodies) {
     Vector2d max = {0, 0};
-    
+
     CBody *planets = bodies->data;
     for (int i=0; i<bodies->len; i++) {
         double x = fabs(planets[i].pos.x);
         double y = fabs(planets[i].pos.y);
-        
+
         if (x>max.x) {
             max.x = x;
         }
